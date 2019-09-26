98142875a3933d7adfc5898d0c82cd0c0bb06ed7
Dudy83 <guillaume.zehren@live.fr>
2019-09-26 11:35:40 +0200

    fixed css bugs

--- main.js
+++ main.js
@@ -27,7 +27,6 @@ let volumeProgress = document.getElementById('progressVolume');
 var song = new Audio();
 var currentSong = 0;
 
-
 function loadSong()
 {
     if(currentSong >= songs.length)
@@ -139,19 +138,3 @@ function clickMuted()
         volumeProgress.style.width = song.volume * 100 + "%";    
     }
 }
-
-// Progress Bar function
-
-$("#songVolume").mousemove(function (e) {
-    var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
-    var percent = val * 100;
-
-    $(this).css('background-image',
-        '-webkit-gradient(linear, left top, right top, ' +
-        'color-stop(' + percent + '%, #df7164), ' +
-        'color-stop(' + percent + '%, #F5D0CC)' +
-        ')');
-
-    $(this).css('background-image',
-        '-moz-linear-gradient(left center, #DF7164 0%, #DF7164 ' + percent + '%, #F5D0CC ' + percent + '%, #F5D0CC 100%)');
-});
